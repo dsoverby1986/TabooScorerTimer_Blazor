56963c4e85af8bc31c5588e1e8492d373e1d3b26
Shane Overby <soverby@jackrabbittech.com>
2019-08-28 17:02:12 -0400

    Created scorer-timer.js; added js interop to ScorerTtimer.razor; now have timer alarm working

---
+++ TabooScorerTimer/wwwroot/scripts/scorer-timer.js
@@ -0,0 +1,13 @@
+﻿let timer = undefined;
+
+window.soundAlert = () => {
+    clearInterval(timer);
+    var context = new AudioContext();
+    var o = context.createOscillator();
+    o.type = "sawtooth";
+    o.connect(context.destination);
+    o.start();
+    setTimeout(function () {
+        o.stop();
+    }, 5000);
+}
